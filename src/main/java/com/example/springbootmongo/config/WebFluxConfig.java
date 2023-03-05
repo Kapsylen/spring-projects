1c3bb55e3e4b5fb414b98fba0f94a76c2f1363c0
Sebastian <sdp.svensson@gmail.com>
2023-03-05 16:11:20 +0100

    add docker-compose config

--- src/main/java/com/example/springbootmongo/config/WebFluxConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootmongo.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.EnableWebFlux;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
-
-@Configuration
-@EnableWebFlux
-public class WebFluxConfig implements WebFluxConfigurer {}
